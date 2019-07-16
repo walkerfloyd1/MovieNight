0b98cade48032b69a66c236f1b5b81dd5bae7c39
Walker Floyd <walkerkeatley@yahoo.com>
2019-07-16 11:36:19 -0700

    words display

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render () {
+        return (
+            <div className="Introduction">
+            <h1> Welcome to MovieNight</h1>
+                <br></br>
+            <h1> Please make a selection below below to start your ultimate movie date night.</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;

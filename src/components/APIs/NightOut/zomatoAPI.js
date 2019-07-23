3f8d3e5b9db727e735eef5a00f9adf4af530eff6
Walker Floyd <walkerkeatley@yahoo.com>
2019-07-22 20:56:18 -0700

    zomato api not fully working yet

--- src/components/APIs/NightOut/zomatoAPI.js
+++ src/components/APIs/NightOut/zomatoAPI.js
@@ -14,13 +14,22 @@ class ZomatoResults extends React.Component {
         const config = { headers: {'user-key': '2ffbbf3d242039cdbc944efbd700bd6e'} }; 
         axios.get('https://developers.zomato.com/api/v2.1/geocode?lat=34.1562756&lon=-118.1236828', config)
         .then(response => {
-            console.log(response);
-        }
-        )
+            console.log(response)
+        }).then(data => {
+            let restaurants = data.nearby_restaurants.map((restaurant) => {
+                return (
+                    <ul>
+                        <li>{restaurant.restaurant.name}</li>
+                    </ul>
+                )
+            })
+        this.setState({restaurants: restaurants})
+        console.log("restaurants", restaurants)
+        })
     }
     render () {
         return (
-            <h1>This is where the results will go</h1>
+            <h1>{this.state.restaurants}</h1>
         )
     }
 }

0ab937540822134d7e4b6a936ff836bd87b6a230
Walker Floyd <walkerkeatley@yahoo.com>
2019-07-23 16:07:07 -0700

    all API calls working

--- src/components/APIs/NightIn/utellyAPIResults.js
+++ src/components/APIs/NightIn/utellyAPIResults.js
@@ -11,22 +11,39 @@ const Text = styled.text`
 `;
 
 class UtellyResults extends Component {
+    constructor () {
+        super();
+        this.state = {
+            streamingSites: [],
+        }
+    }
     // this API call isn't working yet
     componentWillMount() {
-        let header = {
+        const config = { headers: {
             "X-RapidAPI-Key": "3a2f6b098amsh42e3ea8a97b54f0p18445ajsn3caa2d5e9ca6",
             "X-RapidAPI-Host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
         }
+        }
         //hard-coded, will need to figure out how to put in the users input
-        axios.get("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=us", {header})
-        .then(response => {
-            console.log(response)
+        axios.get("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=us", config)
+        .then(res => {
+            let streamingSites = res.data.results.map((movie) => {
+                return (
+                    <ul>
+                    <li>{movie.name}</li>
+                    </ul>
+                )
+            })
+            this.setState({
+                streamingSites: streamingSites
+            })
+            console.log("movies", this.state.streamingSites)
         })
     }
 
     render() {
         return (
-            <Text>This is where the Streaming results will go</Text>
+            <div>{this.state.streamingSites}</div>
         )
     }
 }

380e99dd81eeb859e7fd2e19a22a982f26dddedf
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-06 12:47:10 -0700

    server file and added routes

--- server/server.js
+++ server/server.js
@@ -1,24 +1,47 @@
-var express = require("express");
+const express =  require("express");
+const cors = require("cors");
+const path = require("path");
+const logger = require("morgan");
 
-var app = express();
-var PORT = process.env.PORT;
+const app = express();
 
-var db = require("./models");
+const users = require('./routes/users')
 
+const port = process.env.PORT || 3001;
+const db = require("./models");
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+console.log(process.env.SECRET)
+app.use(cors())
+app.use(cookieParser(process.env.SECRET));
+
+// // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Static directory
+// Serve up static assets ( on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../client/build"));
+}
+// make public a static folder
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
+app.use(require('./routes'));
+// // morgan logger for logging requests
+app.use(logger("dev"));
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
+// // API routes here
+
+// // Send every other request to the React app
+// // Define any API routes before this runs
+
+app.get('/*', function (req, res, next) {
+    res.render('home', {
+    });
 });
+
+db.sequelize.sync({force: false}).then(() => {
+    app.listen(port, () => {
+        console.log("server is running on port: " + port)
+    })
+})

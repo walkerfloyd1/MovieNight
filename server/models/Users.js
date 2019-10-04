c4eb55c3535b7c28241030a15db0676388e0d59a
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-04 09:27:19 -0700

    backend

--- server/models/Users.js
+++ server/models/Users.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
         required: true
@@ -12,4 +16,4 @@ const UserSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model("Users", UserSchema)
+module.exports = mongoose.model("users", UserSchema)

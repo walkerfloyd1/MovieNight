1be885999e5d2e0a4b574330a4c635a79143a442
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-06 17:42:22 -0700

    users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -37,9 +37,47 @@ router.post('/', [
             username
         })
         
-    } catch (error) {
+        if (user) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'A user already exists with this email'
+                }
+                ]
+            })
+        }
+
+        user = new User({
+            name,
+            email,
+            username,
+            password
+        })
+
+        const jwtSalt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,jwtSalt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        ( err, token ) => {
+            if (err) throw err;
+            res.json({ token })
+        });
         
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server error')
     }
-})
+});
 
 module.exports = router;

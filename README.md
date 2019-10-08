96bb4d27ae9ee1f3dd9fc495695c83dc5567a57b
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-07 18:23:23 -0700

    restructured

--- README.md
+++
@@ -1,3 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-The goal of this app is to generate a suggested movie for you to spend your movie night watching, as well as provide snack options and other things to make sure that your MovieNight is as good as possible!
--- client/src/components/APIs/NightOut/zomatoAPI.js
+++ client/src/components/APIs/NightOut/zomatoAPI.js
@@ -10,7 +10,6 @@ import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
 // const useStyles = makeStyles(theme => ({

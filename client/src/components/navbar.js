d723220af72be6edc71365b5450f5696b1c29cb1
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-14 13:40:31 -0700

    reset navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,19 +4,7 @@ import { Link, Redirect } from "react-router-dom";
 
 import styled from 'styled-components';
 
-import { Navbar } from 'reactstrap';
-
-const NavbarDropdown = styled.div`
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0, 2);
-  padding: 12px 16px;
-  z-index: 1;
-`;
-
-const StyledNavbar = styled.div`
+const Menu = styled.div`
   
   ul {
     height: 50px;
@@ -50,44 +38,61 @@ const StyledNavbar = styled.div`
   }
 `;
 
-const listItem = styled.li`
-  position: relative;
-  display: block;
-  &:hover ${NavbarDropdown} {
-    display: block;
-  };
-`;
-
-class TopNav extends React.Component {
+class Navbar extends React.Component {
   constructor() {
     super()
   }
 
+
   render(){
     return (
-      <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
+      <ResponsiveMenu
+        menuOpenButton={<div />}
+        menuCloseButton={<div />}
+        changeMenuOn="800px"
+        largeMenuClassName="large-menu-classname"
+        smallMenuClassName="small-menu-classname"
+        menu={
+          <Menu>
+          <ul>
+          <li className="nav-item">
+              <Link to="/nightin" className={window.location.pathname === "/nightin" ? "nav-link active" : "nav-link"}>
+                Night In
+              </Link>
+          </li>
+            <li className="nav-item">
+                {/* haven't written the code for the page yet */}
+              <Link
+            to="/nightout"
+            className={window.location.pathname === "/nightout" ? "nav-link active" : "nav-link"}
+              >
+                Night Out
+              </Link>
+            </li>
+            <li className="nav-item">
+                {/* haven't written the code for the page yet */}
+              <Link
+                to="/profile"
+                className={window.location.pathname === "/profile" ? "nav-link active" : "nav-link"}
+              >
+                Profile
+              </Link>
+           </li>
+        <li className="nav-item">
+          <Link
+            to="/signin"
+            className={window.location.pathname === "/signin" ? "nav-link active" : "nav-link"}  
+            onClick={this.logout}
+          >
+            Logout
+          </Link>
+          </li>
+          </ul>
+          </Menu>
+        }
+        />  
     );
   }
 }
 
-export default TopNav;
+export default Navbar;

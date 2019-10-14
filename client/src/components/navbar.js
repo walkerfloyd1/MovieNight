04b8b13cd8f6ebb62ad309a8df5612f207fc1ced
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-14 12:33:02 -0700

    random

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,6 +4,8 @@ import { Link, Redirect } from "react-router-dom";
 
 import styled from 'styled-components';
 
+import { Navbar } from 'reactstrap';
+
 const NavbarDropdown = styled.div`
   display: none;
   position: absolute;
@@ -56,57 +58,36 @@ const listItem = styled.li`
   };
 `;
 
-class Navbar extends React.Component {
+class TopNav extends React.Component {
   constructor() {
     super()
   }
 
   render(){
     return (
-
-          <StyledNavbar>
-          <ul>
-          <listItem>
-              <h1>
-                Night In
-              </h1>
-              <NavbarDropdown>
-                <Link to="/nightin" className={window.location.pathname === "/nightin" ? "nav-link active" : "nav-link"}>
-                  Night In
-                </Link>
-              </NavbarDropdown>
-          </listItem>
-            <li className="nav-item">
-                {/* haven't written the code for the page yet */}
-              <Link
-            to="/nightout"
-            className={window.location.pathname === "/nightout" ? "nav-link active" : "nav-link"}
-              >
-                Night Out
-              </Link>
-            </li>
-            <li className="nav-item">
-                {/* haven't written the code for the page yet */}
-              <Link
-                to="/profile"
-                className={window.location.pathname === "/profile" ? "nav-link active" : "nav-link"}
-              >
-                Profile
-              </Link>
-           </li>
-        <li className="nav-item">
-          <Link
-            to="/signin"
-            className={window.location.pathname === "/signin" ? "nav-link active" : "nav-link"}  
-            onClick={this.logout}
-          >
-            Logout
-          </Link>
-          </li>
-          </ul>
-          </StyledNavbar> 
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
     );
   }
 }
 
-export default Navbar;
+export default TopNav;

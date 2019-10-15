946adc20c3ca79661f9ee903b7b0f680d849d6b5
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-15 14:31:43 -0700

    added redux files

---
+++ client/src/redux/auth/Login.js
@@ -0,0 +1,18 @@
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Login = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Login.propTypes = {
+
+}
+
+export default Login

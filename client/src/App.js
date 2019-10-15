89ae6527f5e70dbc883d87b219f642531a1749b5
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-15 15:07:42 -0700

    working on redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Fragment,useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 import Landing from "./pages/home.js";
-import Signin from './pages/signin.js';
+import Login from './redux/auth/Login';
 import Register from './pages/register.js';
 import Profile from './pages/profile.js';
 import NightInFood from './pages/NightIn/NightInFood';
@@ -9,23 +9,35 @@ import NightInMovie from './pages/NightIn/NightInMovie';
 import NightOutTheaters from './pages/NightOut/NightOutTheaters';
 import NightOutFood from './pages/NightOut/NightOutFood';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { loadUser } from './redux/actions/auth';
+import setAuthToken from './redux/utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 
-class App extends React.Component {
-  render() {
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
     return (
+      <Provider store={store}>
       <Router>
         <Route exact path="/" component={Landing}/>
         <Route exact path="/recipes" component={NightInFood}/>
         <Route exact path="/movie" component={NightInMovie}/>
-        <Route exact path="/signin" component={Signin}/>
+        <Route exact path="/signin" component={Login}/>
         <Route exact path="/register" component={Register}/>
         <Route exact path="/food" component={NightOutFood}/>
         <Route exact path="/theaters" component={NightOutTheaters}/>
         <Route exact path="/profile" component={Profile}/>
       </Router>
+      </Provider>
     )
-  }
 }
 
 export default App;
--- client/src/redux/auth/Login.js
+++ client/src/redux/auth/Login.js
@@ -2,17 +2,94 @@ import React, { Fragment, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { login } from '../actions/auth';
+
+import styled from 'styled-components';
+import { Container } from '../../components/homeContainer';
+import Form from 'react-bootstrap/Form';
+
+const Username = styled.text`
+        text-decoration: none;
+        padding-bottom: 10px;
+        margin-bottom: 60px;
+        font-size: 25px;
+        color: black;
+`;
+
+const Password = styled.text`
+        text-decoration: none;
+        padding-bottom: 10px;
+        margin-bottom: 60px;
+        font-size: 25px;
+        color: black;
+`;
+
+const SignInButton = styled.button`
+    color: black;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid black;
+    border-radius: 3px;
+        `;
+
+const Input = styled.input`
+    padding: 0.5em;
+    width: 100%;
+    color: ${props => props.inputColor || "black"};
+    background: white;
+    border-bottom: solid black;
+    border-right: gray;
+    border-top: gray;
+    border-left: gray
+    border-radius: 3px;
+    `;
+
 
 const Login = props => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+      });
+
+    const { username, password } = formData;
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        login(username, password);
+      };
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Form onSubmit={e => onSubmit(e)}>
+  <Form.Group controlId="formBasicEmail">
+    <Username>Username</Username>
+    <Input type="username" placeholder="Enter username" name="username" value={username} onChange={e => onChange(e)} required/>
+  </Form.Group>
+  <Form.Group controlId="formBasicPassword">
+    <Password>Password</Password>
+    <Input type="password" placeholder="Password" name="password" minLength="7" value={password} onChange={e => onChange(e)} required/>
+  </Form.Group>
+        <Input type="submit" value="Login" />
+  <SignInButton variant="primary" type="submit" onClick={this.register}>
+    Submit
+  </SignInButton>
+  </Form>
+  <p className="my-1">
+    Don't have an account? <Link to="/register">Sign up</Link>
+  </p> 
+            </Container>
     )
 }
 
 Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+};
 
-}
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
 
-export default Login
+export default connect(mapStateToProps, { login })(Login)
